81ed05b236a247ef1be21150bc80be7a8f917657
slfan2013 <fansili2013@gmail.com>
2016-08-16 00:52:40 -0700

    finer single_population

--- inst/www/js/Single_Population.js
+++ inst/www/js/Single_Population.js
@@ -9,56 +9,108 @@ app.controller('populationplotctrl_singlepopulation', function($scope) {
   $scope.max = parseInt(localStorage.getItem("largestvalue_singlepopulation"),10);
   $scope.mean = parseInt(localStorage.getItem("mean_singlepopulation"),10);
   $scope.sd = parseInt(localStorage.getItem("sd_singlepopulation"),10);
-  $scope.populationplot
+  $scope.displaybottom = false;
+  $scope.n = 5;
+  $scope.repeat = 1;
+  $scope.sampleselected = null;
 
+  $scope.sample = function(){
+    var sampleset = [];
+    var samplemeans = [];
+    var samplesds = [];
+    var ys = [];
+    for(ii=0;ii<$("#sampletimes_singlepopulation").val();ii++){
+      rdm = rnorm($("#samplesize_singlepopulation").val(),mean=$scope.mean,sd=$scope.sd)
+      sampleset.push(rdm)
+      samplemeans.push({value:meanFunction(rdm), name:ii+"thsample_singlepopulation"});
+      samplesds.push({value:sdFunction(rdm),name:ii+"thsample_singlepopulation"})
+    }
+    $scope.sampleset = sampleset;
+    $scope.samplemeans = samplemeans;
+    $scope.samplesds = samplesds;
+  }
 
-
-  $scope.populationplot = function() {
-
-    var x = seq($scope.min, $scope.max, length=1000)
+  $scope.populationplot = function(){
+    var x = seq($scope.min, $scope.max, length=1000);
     var y = pnorm(x,$scope.mean,$scope.sd)
-    value = {x: x,y: y,type: 'scatter',name : 'density',   fill: 'tonexty'}
-    mean = {x:[$scope.mean,$scope.mean], y:[0,Math.max.apply(null, y)*1.1],mode: 'lines',name :'average'}
-    var data = [value,mean];
-
-    var layout = {xaxis: {range: [$scope.min, $scope.max]},yaxis: {range: [Math.min.apply(null, y), Math.max.apply(null, y)*1.1]},paper_bgcolor:'rgba(0,0,0,0)',plot_bgcolor:'rgba(0,0,0,0)'};
+    x.push($scope.max);x.push($scope.mean);x.push($scope.mean);x.push($scope.mean);x.push($scope.min);
+    y.push(0);y.push(0);y.push(Math.max.apply(null, y)*1.1);y.push(0);y.push(0)
+    value = {x: x,y: y,type: 'scatter',name : 'density.true', fill: 'tozeroy',"xaxis": "x1","yaxis": "y1"}
+    var data = [value];
+    $scope.truevalue = value;
+        var layout = {"barmode": "overlay",
+                    xaxis1: {range: [$scope.min, $scope.max],"anchor": "y2","domain": [0.0, 1.0],"zeroline": false},
+                    "yaxis1": {"anchor": "free", "domain": [ 0.05, 1], "position": 0.0},
+                    "yaxis2": {"anchor": "x1", "domain": [0,0.1], "dtick": 1, "showticklabels": false},
+                    paper_bgcolor:'rgba(0,0,0,0)',plot_bgcolor:'rgba(0,0,0,0)'}
 
     $scope.populationplot_singlepopulation = Plotly.newPlot('populationplot_singlepopulation', data,layout);
   };$scope.populationplot()
 
-    $scope.samplepopulation = function(){
-
-      var sampleset = [];
-      var samplemeans = [];
-      var samplesds = [];
-      var ys = [];
-
-      for(ii=0;ii<$("#sampletimes_singlepopulation").val();ii++){
-        rdm = rnorm($("#samplesize_singlepopulation").val(),mean=$scope.mean,sd=$scope.sd)
-        sampleset.push(rdm)
-        samplemeans.push(meanFunction(rdm))
-        samplesds.push(sdFunction(rdm))
-      }
-
-      $scope.samplemeans = samplemeans;
-      $scope.samplesds = samplesds;
-
-
-/* need to change to hover event.
-      var data = [];
-      var mean = [];
-      var x = seq($scope.min, $scope.max, length=1000)
-      var truey = pnorm(x,$scope.mean,$scope.sd)
-      for(ii=0;ii<$("#sampletimes_singlepopulation").val();ii++){
-        y = pnorm(x,samplemeans[[ii]],samplesds[[ii]])
-        data.push({x:x,y:y,type: 'scatter',name : 'density'+ii,fill: 'tonexty'},{x:[samplemeans[[ii]],samplemeans[[ii]]], y:[0,Math.max.apply(null, y)*1.1],mode: 'lines',name :'average'+ii});
-        var layout = {
-                        xaxis: {range: [$scope.min, $scope.max]},yaxis: {range: [Math.min.apply(null, truey), Math.max.apply(null, truey)*1.1]},
-                        paper_bgcolor:'rgba(0,0,0,0)',plot_bgcolor:'rgba(0,0,0,0)'
-                      };
-        $scope.samplepopulationplot_singlepopulation = Plotly.newPlot('samplepopulationplot_singlepopulation', data,layout);
-      }*/
+  // click Sample! and draw plot if repeat one time. Otherwise, need users to mouse over.
+  $scope.samplepopulation = function(){
+    if($("#sampletimes_singlepopulation").val()>1){
+      $scope.displaybottom = true;
+    }else{
+      $scope.displaybottom = false;
     }
+    $scope.sample();
+    var data = [];
+    var mean = [];
+    var x = seq($scope.min, $scope.max, length=1000)
+    data.push($scope.truevalue);
+
+    for(ii=0;ii<$("#sampletimes_singlepopulation").val();ii++){
+      var y = pnorm(x,$scope.samplemeans[[ii]].value,$scope.samplesds[[ii]].value)
+      data.push([{x:x,y:y,type: 'scatter',name : 'density.sample'+ii,fill: 'tozeroy',opacity: 0.75,"xaxis": "x1","yaxis": "y1"},
+      {x:$scope.sampleset[[ii]],y:rep('data.sample'+ii,$("#samplesize_singlepopulation").val()),name:'data.sample'+ii,"showlegend": false,"type": "scatter","mode": "markers",
+      "marker": {"color": "rgb(255, 127, 14)","symbol": "line-ns-open"},"xaxis": "x1","yaxis": "y2"},
+      {x:[$scope.samplemeans[ii].value,$scope.samplemeans[ii].value], y:[0,Math.max.apply(null, y)*1.1],mode: 'lines',name :'sample.average',marker:{color:'#cc6600'}}]);
+    }
+    var layout = {"barmode": "overlay",
+                    xaxis1: {range: [$scope.min, $scope.max],"anchor": "y2","domain": [0.0, 1.0],"zeroline": false},
+                    "yaxis1": {"anchor": "free", "domain": [ 0.05, 1], "position": 0.0},
+                    "yaxis2": {"anchor": "x1", "domain": [0,0.1], "dtick": 1, "showticklabels": false},
+                    paper_bgcolor:'rgba(0,0,0,0)',plot_bgcolor:'rgba(0,0,0,0)',
+                    annotations: [{x: $scope.samplemeans[0].value,y: 0,xref: 'x',yref: 'y',text: $scope.samplemeans[0].value,showarrow: true,arrowhead: 2,ax: 20,ay: -30}]
+                  };
+    $scope.sampledata = data;
+    $scope.samplelayout = layout;
+    $scope.samplepopulationplot_singlepopulation = Plotly.newPlot('populationplot_singlepopulation',
+            [data[0],data[1][0],data[1][1],data[1][2]],layout);
+
+    $scope.samplesetplot()
+  }
+
+
+  $scope.samplesetplot = function(highlight_index = null){
+    var x = Array.from(Array($scope.repeat).keys());
+    var y = $scope.samplemeans.map(function(a) {return parseFloat(a.value);})
+    data = [{x:[0,$scope.repeat-1],y:[$scope.mean,$scope.mean],name:"average",mode: 'lines'},{x:x,y:y,type:'scatter',mode: 'lines+markers',name:"estimated average"}]
+    if(highlight_index!==null){
+      data.push({x:[highlight_index],y:[parseFloat($scope.samplemeans[highlight_index].value)], type:'scatter', name:"highlighted point", marker:{color:"red"},mode: 'markers'})
+    }
+    Plotly.newPlot('samplesetplot_singlepopulation', data,{yaxis:{range:[$scope.min, $scope.max]}});
+  }
+
+  //hover event.
+
+  $scope.setSelected = function(sampleselected) {
+  $scope.sampleselected = sampleselected;
+  data = $scope.sampledata[extractNumber($scope.sampleselected)+1]
+  var layout = {"barmode": "overlay",
+              xaxis1: {range: [$scope.min, $scope.max],"anchor": "y2","domain": [0.0, 1.0],"zeroline": false},
+              "yaxis1": {"anchor": "free", "domain": [ 0.05, 1], "position": 0.0},
+              "yaxis2": {"anchor": "x1", "domain": [0,0.1], "dtick": 1, "showticklabels": false},
+              paper_bgcolor:'rgba(0,0,0,0)',plot_bgcolor:'rgba(0,0,0,0)',
+              annotations: [{x: $scope.samplemeans[extractNumber($scope.sampleselected)].value,y: 0,xref: 'x',yref: 'y',text: $scope.samplemeans[extractNumber(sampleselected)].value,showarrow: true,arrowhead: 2,ax: 20,ay: -30}]
+        };
+  $scope.samplepopulationplot_singlepopulation = Plotly.newPlot('populationplot_singlepopulation',
+           [$scope.truevalue,data[0],data[1],data[2]],layout);
+  $scope.samplesetplot(extractNumber($scope.sampleselected))
+}
+
+
 });
 
 
--- inst/www/js/myJS.js
+++ inst/www/js/myJS.js
@@ -21,34 +21,57 @@
     }
     return(result)
   }
-  sumFunction = function(x){
+  sumFunction = function(x,decimal = 2){
     var result = 0;
     for( var i = 0; i < x.length; i++ ){
       result += parseInt( x[i], 10 ); //don't forget to add the base
     }
-    return(result)
+    return(result.toFixed(decimal))
   }
-  meanFunction = function(x){
+  meanFunction = function(x,decimal = 2){
     var result = 0;
     for( var i = 0; i < x.length; i++ ){
       result += parseFloat( x[i], 10 ) //don't forget to add the base
     }
-    return(result/(x.length))
+    result = result/(x.length)
+    return(result.toFixed(decimal))
   }
-  sdFunction = function(x){
+  sdFunction = function(x,decimal = 2){
     var avg = meanFunction(x);
     var result = 0;
     var diff = [];
     for(var i=0; i<x.length; i++){
       diff.push(Math.pow((x[i] - avg),2));
     }
-    return(Math.sqrt(meanFunction(diff)))
+    return(Math.sqrt(Number((meanFunction(diff)))+0.01).toFixed(decimal))
 
   }
   storeItemAndReset = function(id){
     localStorage.setItem(id, $('#'+id).val());
     document.getElementById(id).value = localStorage.getItem(id);
   }
+  qnorm = function(p,mean,sd){//require gaussian.js
+    var distribution = gaussian(mean, Math.pow(sd, 2));
+    var result = [];
+    for(i=0;i<p.length;i++){
+        result.push(distribution.ppf(p[i]))
+    }
+    return(result)
+  }
+
+  rep = function(value, len) {
+  var arr = [];
+  for (var i = 0; i < len; i++) {
+    arr.push(value);
+  }
+  return arr;
+}
+  extractNumber = function(txt){
+    var numb = txt.match(/\d/g);
+    numb = numb.join("");
+    return(parseFloat(numb))
+  }
+
 
 $(document).ready(function(){
   $("#test").click(function(){
